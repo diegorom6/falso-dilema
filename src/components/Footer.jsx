6dfb4d0e9750fdbb8afc73da9a06839003192a4c
diegorom6 <diego.romero.v13@gmail.com>
2023-06-19 15:03:53 -0400

    Fix margin footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,35 @@
 import React from "react";
-import { FaTiktok, FaInstagram, FaSpotify, FaSoundcloud, FaAmazon, FaDeezer, FaItunes, FaYoutube, FaApple, } from "react-icons/fa";
+import {
+    FaTiktok,
+    FaInstagram,
+    FaSpotify,
+    FaSoundcloud,
+    FaAmazon,
+    FaDeezer,
+    FaItunes,
+    FaYoutube,
+    FaApple,
+} from "react-icons/fa";
 import { SiTidal, SiShazam } from "react-icons/si";
 
 const Footer = () => {
     return (
-        <section>
-            <div className="footer">
-                <div className="media rrss">
-                    <FaTiktok />
-                    <FaInstagram />
-                    <FaYoutube />
-                </div>
+        <section className="footer">
+            <div className="media rrss">
+                <FaTiktok />
+                <FaInstagram />
+                <FaYoutube />
+            </div>
 
-                <div className="media stream">
-                    <FaSpotify />
-                    <FaApple />
-                    <FaSoundcloud />
-                    <FaItunes />
-                    <SiShazam />
-                    <FaAmazon />
-                    <SiTidal />
-                    <FaDeezer />
-                </div>
+            <div className="media stream">
+                <FaSpotify />
+                <FaApple />
+                <FaSoundcloud />
+                <FaItunes />
+                <SiShazam />
+                <FaAmazon />
+                <SiTidal />
+                <FaDeezer />
             </div>
         </section>
     );
