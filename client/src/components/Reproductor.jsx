65312433c8e3c99c3e3ab61a789f81aeea814a23
diegorom6 <diego.romero.v13@gmail.com>
2023-07-30 13:08:42 -0400

    new video

--- client/src/components/Reproductor.jsx
+++ client/src/components/Reproductor.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 
 const Reproductor = () => {
-  return (
-    <section className="player">
-        <iframe style={{borderRadius: "8px", border: "0px"}} src="https://open.spotify.com/embed/artist/7CUvcQZsnpQo8bUR04Wxo6?utm_source=generator&theme=0" width="100%" height="470" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    </section>
-  )
-}
+    return (
+        <>
+            <section className="video-embed">
+                <iframe
+                    width="80%"
+                    height="700"
+                    src="https://www.youtube.com/embed/N-m7xCIN_3w"
+                    title="YouTube video player"
+                    style={{ border: "0px" }}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen
+                ></iframe>
+            </section>
 
-export default Reproductor
+            <section className="player">
+                <iframe
+                    style={{ borderRadius: "8px", border: "0px" }}
+                    src="https://open.spotify.com/embed/artist/7CUvcQZsnpQo8bUR04Wxo6?utm_source=generator&theme=0"
+                    width="100%"
+                    height="470"
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                    loading="lazy"
+                ></iframe>
+            </section>
+        </>
+    );
+};
+
+export default Reproductor;
